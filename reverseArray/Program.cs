e61847a74070cc467fd7e37834a328555f0816fc
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-06-23 13:45:25 -0400

    Agregados ejercicios del día

---
+++ reverseArray/Program.cs
@@ -0,0 +1,19 @@
+﻿string str = "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int letterCount = 0;
+
+foreach (char letter in charMessage) 
+{
+    if (letter == 'o')
+    {
+        letterCount++; 
+    } 
+}
+
+string new_message = new String(charMessage);
+
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {letterCount} times.");
