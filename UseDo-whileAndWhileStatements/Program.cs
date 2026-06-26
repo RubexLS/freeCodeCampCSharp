788d77f4635d218c44c7c835a6f12bb92ee3740f
Ruben Alex Lique Sarzuri <rubenalexlsdev@gmail.com>
2026-06-25 21:44:43 -0400

    ejercicio 26_06_2026

---
+++ UseDo-whileAndWhileStatements/Program.cs
@@ -0,0 +1,58 @@
+﻿int healthMonster = 10;
+int healthHero = 10;
+
+Random hit = new Random();
+int i = 0;
+
+do
+{
+	i++;
+	int damage = hit.Next(1, 10);
+	if(i % 2 != 0)
+	{
+		healthMonster -= damage;
+		if(healthMonster >= 0)
+			Console.WriteLine($"Monster was damaged and lost {damage} health and now has {healthMonster} health.");
+		else
+			Console.WriteLine($"Monster was damaged and lost {damage} health and now has 0 health.");
+	}
+	else
+	{
+		healthHero -= damage;
+		if(healthHero >= 0)
+			Console.WriteLine($"Hero was damaged and lost {damage} health and now has {healthHero} health.");
+		else
+			Console.WriteLine($"Hero was damaged and lost {damage} health and now has 0 health.");
+	}
+}while(healthMonster > 0 && healthHero > 0);
+
+if(healthMonster < 1)
+	Console.WriteLine("Hero wins!");
+
+else if(healthHero < 1)
+	Console.WriteLine("Monster wins!");
+
+// Alternative solution --------------------------------------------------------------------------
+
+/*
+int hero = 10;
+int monster = 10;
+
+Random dice = new Random();
+
+do
+{
+    int roll = dice.Next(1, 11);
+    monster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+    if (monster <= 0) continue;
+
+    roll = dice.Next(1, 11);
+    hero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+} while (hero > 0 && monster > 0);
+
+Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
+*/
---
+++ differenceBetweenWhileAndDo-while/Program.cs
@@ -0,0 +1,80 @@
+﻿//CodeCampfree - Add Looping Logic to Your Code Using the do-while and while Statements in C#
+
+//exercise 1---------------------------------------------------------------------------------------
+
+int numericValue = 0;
+bool validNumber = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10.");
+
+do
+{
+    string? readValue = Console.ReadLine();
+    validNumber = int.TryParse(readValue, out numericValue);
+    if(validNumber == false)
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+        continue;
+    }
+    else
+    {    
+        if(numericValue < 5 || numericValue > 10)
+        {
+            Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
+            validNumber = false;
+        }
+    }
+}while(validNumber == false);
+
+Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+
+
+//exercise 2---------------------------------------------------------------------------------------
+
+
+bool validRole = false;
+
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+
+do
+{
+    string? roleInput = Console.ReadLine();
+    if(roleInput == null)
+    {
+        Console.WriteLine($"The role name that you entered, \"{roleInput}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+        continue;
+    }
+    else
+    {
+        string roleClean = roleInput.Trim().ToLower();
+        if(roleClean != "administrator" && roleClean != "manager" && roleClean != "user")
+            Console.WriteLine($"The role name that you entered, \"{roleClean}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+            
+        else
+        {
+            validRole = true;
+            Console.WriteLine($"Your input value ({roleClean}) has been accepted.");
+        }
+    }
+}while(validRole ==false);
+
+
+//exercise 3---------------------------------------------------------------------------------------
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+foreach(string x in myStrings)
+{
+    string myString = x;
+    int periodLocation = myString.IndexOf('.'); 
+    while(periodLocation != -1)
+    {
+        string result = myString.Substring(0, periodLocation);
+        Console.WriteLine(result);
+        myString = myString.Remove(0, periodLocation+1);
+        myString = myString.TrimStart();
+        periodLocation = myString.IndexOf('.');
+    }
+    
+    Console.WriteLine(myString);
+}
